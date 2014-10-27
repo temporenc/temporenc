70de5d910451438536c6df7082b32f055e373849
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-27 17:33:49 +0100

    Add very basic Flask app that renders the template

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
