621e63992e1d0c532c203c0f4d13cf53ea76d8f9
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-29 23:41:52 +0100

    Revise parts of the ReST to HTML conversion

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-import collections
 import io
-import itertools
 
 from docutils.core import publish_parts
 from flask import Flask, Markup, render_template
-from flask.ext.frozen import Freezer
+# from flask.ext.frozen import Freezer
 from lxml import etree
 
 
@@ -38,12 +36,22 @@ def elements_as_html(elements):
 
 
 def inner_html(element):
-    chunks = [element.text or '']
+    chunks = []
+
+    if element.text:
+        chunks.append(element.text)
+
     chunks.extend(
         etree.tostring(child, encoding='unicode', method='html')
         for child in element
     )
     return Markup(''.join(chunks))
+
+
+def bump_headings(element):
+    d = dict(('h%d' % (n+1), 'h%d' % n) for n in range(1, 6))
+    for sub in element:
+        sub.tag = d.get(sub.tag, sub.tag)
 
 
 def flatten_sections(element):
@@ -86,16 +94,11 @@ def transform_spec(source):
     parts = format_rst(source)
     body = parse_html(parts['body']).getroot().find('body')
     doc = flatten_sections(body)
-
-    # Extract all top level sections
-    # sections = collections.OrderedDict()
-    # for section in body:
-    #     assert section.tag == 'div' and section.get('class') == 'section'
-    #     sections[section.get('id')] = section
+    bump_headings(doc)
 
     # Extract short feature descriptions
     tmp = etree.Element('tmp')
-    tmp.extend(extract_section(doc, doc.find('.//*[@id="features-short"]')))
+    tmp.extend(extract_section(doc, doc.find('.//*[@id="features"]')))
     tmp.remove(tmp[0])  # drop heading
     features = []
     while len(tmp):
@@ -106,16 +109,25 @@ def transform_spec(source):
         ))
     assert len(features) == 3
 
+    # Extract all top level sections
+    sections = []
+    while len(doc):
+        section = extract_section(doc, doc[0])
+        sections.append(dict(
+            id=section[0].get('id'),
+            title=inner_html(section[0]),
+            body=elements_as_html(section[1:]),
+        ))
+
     # Join together all other sections for display in the template
-    # FIXME: do fancier stuff
-    # text = Markup('').join(map(element_as_html, sections.values()))
-    text = element_as_html(doc)
+    # text = element_as_html(doc)
 
     return dict(
         title=parts['title'],
         subtitle=parts['subtitle'],
         features_short=features,
-        body=text,
+        # body=text,
+        sections=sections,
     )
 
 
@@ -123,11 +135,8 @@ def transform_spec(source):
 def index():
     with open('README.rst') as fp:
         source = fp.read()
-    doc = transform_spec(source)
-    return render_template(
-        'index.html',
-        doc=doc,
-    )
+    context = transform_spec(source)
+    return render_template('index.html', **context)
 
 
 if __name__ == '__main__':
